73b2f5335dd8d8bbc379b568e9a8304d8720a6ce
Namirah anis <namiranish@gmail.com>
2025-01-17 20:46:55 +0530

    completed the task

---
+++ script.js
@@ -0,0 +1,33 @@
+const display = document.getElementById('display');
+const accessKey = 'yaraX0WlCZtrqNNj7e52cks6qAHUZQn0Izt6_BSHedY';
+const url = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=10`;
+
+async function getImages() {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+
+        showImages(data);
+    } catch (err) {
+        console.log('Error fetching images:', err);
+    }
+}
+
+function showImages(data) {
+    data.forEach((ele) => {
+        let img = document.createElement('img');
+        img.src = ele.urls.small;
+        img.alt = ele.alt_description || 'Image';
+        display.appendChild(img);
+    });
+}
+
+getImages();
+
+
+window.addEventListener('scroll',()=>{
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 200){
+        getImages()
+    }
+})
